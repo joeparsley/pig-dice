ebd3a685d66907e55a8b5c311b4c0f11621435fe
Dylan Dills and Joe Parsley <joe@>
2017-05-10 10:03:08 -0700

    show created objects with the class of PigDice

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 //Business Logic
 
 // constructor for Player
+function PigDice (playerName, pigDiceRoll) {
+  this.playerName = "";
+  this.pigDiceRoll = 0;
+}
 
-
+var pigDice = new PigDice();
 // prototypes for Player 1
 
 // prototypes for Player 2
