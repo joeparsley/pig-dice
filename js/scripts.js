506518a18974ab9983d9d23c7467de4cb3e74816
Joe Parsley and Karina Roush <joe@>
2017-05-11 14:25:26 -0700

    add logic to allow the user to hold their current runningTotal and add it to their grandScore

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,14 @@ var Game = function(playerOneScore, playerTwoScore) {
   this.playerTwoScore = playerTwoScore;
   this.winningScore = 100;
 }
-
 // Constructor for Player
-var Player = function(playerName, currentScore, runningScore) {
+var Player = function(playerName, runningScore, grandScore) {
   this.playerName = playerName;
-  this.currentScore = currentScore;
   this.runningScore = runningScore;
   this.turnTotal = [];
+  this.grandScore = grandScore;
+  this.grandTotal = [];
 }
-
 // For loop that adds objects inside array together to create running total
 Player.prototype.addTurnTotal = function(array) {
   var result = 0;
@@ -23,7 +22,6 @@ Player.prototype.addTurnTotal = function(array) {
   }
   this.runningScore = result;
 }
-
 // Clear runningScore if player rolls a 1:
 Player.prototype.ifOne = function(dice) {
   if (dice === 1) {
@@ -33,21 +31,23 @@ Player.prototype.ifOne = function(dice) {
 return this.runningScore;
 return this.turnTotal;
 }
+// adds hold() method to the Dice prototype, which will add running total to grand total
+Player.prototype.hold = function(total) {
+  var result = 0;
+  for (var i = 0; i < total.length; i++) {
+    result += total[i];
+  }
+  this.grandScore = result;
+}
 
 
 // Constructor for Dice
 var Dice = function() {
   this.sides = 6;
 }
-
 // adds roll() method to the Dice prototype, generates random number from 1 to 6
 Dice.prototype.roll = function () {
 return Math.floor(Math.random() * this.sides + 1);
-}
-
-// adds hold() method to the Dice prototype, which will add running total to grand total
-Dice.prototype.hold = function () {
-
 }
 
 
@@ -77,6 +77,18 @@ $(document).ready(function() {
     console.log(playerOne);
   });
 
+// hold Button for playerOne:
+$("#holdOne").click(function(event){
+  event.preventDefault();
+  var test = playerOne.grandTotal.push(playerOne.runningScore)
+  var holdPlayerOne = playerOne.hold(playerOne.grandTotal);
+  $(".grandTotal").text(playerOne.grandScore)
+console.log(holdPlayerOne);
+});
+
+
+
+
 // click function for player 2
     $("#rollTwo").click(function(event) {
       event.preventDefault();
@@ -84,14 +96,24 @@ $(document).ready(function() {
 // rolls dice, pushes result to turnTotal array
       var diceRollTwo = new Dice();
       var currentRollTwo = diceRollTwo.roll();
-      var playerTurn = playerTwo.turnTotal.push(currentRollTwo)
+      var playerTurn = playerTwo.turnTotal.push(currentRollTwo);
       var run = playerTwo.addTurnTotal(playerTwo.turnTotal);
-      var ifitsOne = playerTwo.ifOne(currentRollTwo)
+// if the player rolls a 1, it empties the turnTotal and runningScore
+      var ifitsOne = playerTwo.ifOne(currentRollTwo);
 // displaying results for Player 2 on HTML for user
       $(".roll-result-2").text(currentRollTwo);
       $(".runningTotal2").text(playerTwo.runningScore);
       console.log(currentRollTwo);
       console.log(playerTwo);
   });
+
+// hold button for playerTwo:
+$("#holdTwo").click(function(event){
+  event.preventDefault();
+  var test = playerTwo.grandTotal.push(playerTwo.runningScore);
+  var holdPlayerTwo = playerTwo.hold(playerTwo.grandTotal);
+  $(".grandTotal2").text(playerTwo.grandScore)
+console.log(holdPlayerTwo);
+});
 
 });
